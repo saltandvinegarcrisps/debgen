0e514135329f9895fc3f85194d2971fe0e165de3
Kieron <info@kieronwilson.co.uk>
2016-07-06 21:07:07 +0100

    make it so!

---
+++ public/app.js
@@ -0,0 +1,60 @@
+(function() {
+	var button = document.querySelector('button'),
+		mirror = document.querySelector('select[name=mirror]'),
+		arch = document.querySelector('select[name=arch]'),
+		releases = document.querySelector('select[name=releases]'),
+		list = document.querySelector('textarea[name=list]'),
+		src = document.querySelector('input[name=src]'),
+		contrib = document.querySelector('input[name=contrib]'),
+		nonfree = document.querySelector('input[name=non-free]'),
+		security = document.querySelector('input[name=security]');
+
+	var sourceList = [];
+
+	var getComponents = function() {
+		var components = ['main'];
+
+		if(contrib.checked) components.push('contrib');
+		if(nonfree.checked) components.push('non-free');
+
+		return components.join(' ');
+	};
+
+	var getArch = function() {
+		var value = arch.options[arch.selectedIndex].value;
+		return value ? '[arch=' + value + ']' : '';
+	};
+
+	var appendSource = function(source) {
+		sourceList.push(source.filter(function(element) { return element.length; }).join(' '));
+	};
+
+	var generate = function() {
+		var ftp = mirror.options[mirror.selectedIndex].value,
+			rel = releases.options[releases.selectedIndex].value;
+
+		var comps = getComponents();
+		var arch = getArch();
+
+		appendSource(['deb', arch, ftp, rel, comps]);
+		if(src.checked) appendSource(['deb-src', arch, ftp, rel, comps]);
+
+		if(releases.options[releases.selectedIndex].hasAttribute('data-updates')) {
+			appendSource(['']);
+			appendSource(['deb', arch, ftp, rel + '-updates', comps]);
+			if(src.checked) appendSource(['deb-src', arch, ftp, rel + '-updates', comps]);
+		}
+
+		if(security.checked) {
+			appendSource(['']);
+			appendSource(['deb', arch, 'http://security.debian.org/', rel + '/updates']);
+			if(src.checked) appendSource(['deb-src', arch, 'http://security.debian.org/', rel + '/updates']);
+		}
+
+		list.value = sourceList.join("\n");
+		sourceList = [];
+	};
+
+	button.addEventListener('click', generate, false);
+	generate();
+})();
---
+++ readme.md
@@ -0,0 +1,1 @@
+The source code for debgen.xzy
